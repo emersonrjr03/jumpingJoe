de2066664fd4bba978b94bf157beca1514e9f0d1
emerson.junior <emerson.junior@sankhya.com.br>
2020-07-21 17:01:45 -0300

    adding the show when close script to show healthbar only when close

---
+++ Assets/Scripts/ShowWhenCloser.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowWhenCloser : MonoBehaviour
+{
+	public GameObject objectToHideAndShow;
+	
+    private void OnTriggerEnter(Collider other){
+    	if(other.tag == "Player"){
+    		objectToHideAndShow.SetActive(true);
+    	}
+	}
+	public void OnTriggerExit(Collider other){
+    	if(other.tag == "Player"){
+			objectToHideAndShow.SetActive(false);
+		}
+	}
+}
